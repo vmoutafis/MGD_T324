15879ed04f4a79476fc8e5febc0b4d518bb6527d
Vasilios Moutafis <vmoutafis94@gmail.com>
2024-11-13 11:36:30 +1100

    Lobby and sessions

--- Source/MGD_Template/Private/MGGameInstance.cpp
+++ Source/MGD_Template/Private/MGGameInstance.cpp
@@ -3,6 +3,9 @@
 
 #include "MGGameInstance.h"
 #include "OnlineSubsystemUtils.h"
+#include "OnlineSessionSettings.h"
+#include "GameFramework/GameModeBase.h"
+#include "Online/OnlineSessionNames.h"
 
 void UMGGameInstance::Init()
 {
@@ -30,7 +33,18 @@ void UMGGameInstance::Init()
 
 	// bind to the login complete delegate in the identity interface
 	IdentityInterface->OnLoginCompleteDelegates->AddUObject(this, &UMGGameInstance::OnLoginComplete);
-	
+
+	SessionInterface = OssRef->GetSessionInterface();
+
+	if (!SessionInterface)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Session interface not found."))
+		return;
+	}
+
+	SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UMGGameInstance::OnCreateSessionComplete);
+	SessionInterface->OnSessionUserInviteAcceptedDelegates.AddUObject(this, &UMGGameInstance::OnInviteAccepted);
+	SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this, &UMGGameInstance::OnSessionJoinComplete);
 }
 
 void UMGGameInstance::LoginToEOS()
@@ -53,8 +67,121 @@ void UMGGameInstance::LoginToEOS()
 	UE_LOG(LogTemp, Warning, TEXT("Attempting to login..."))
 }
 
+void UMGGameInstance::HostGame()
+{
+	if (!IsLoggedIn() || !SessionInterface)
+		return;
+
+	FOnlineSessionSettings sessionSettings;
+
+	sessionSettings.bAllowInvites = true;
+	sessionSettings.bIsDedicated = false;
+	sessionSettings.bUsesPresence = true;
+	sessionSettings.NumPrivateConnections = 4;
+	sessionSettings.bIsLANMatch = false;
+	sessionSettings.bUseLobbiesIfAvailable = true;
+
+	if (!SessionInterface->CreateSession(0, TEXT("MGSESSION"), sessionSettings))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Session failed to create synchronously"))
+		return;
+	}
+
+	UE_LOG(LogTemp, Error, TEXT("Attempting to host session..."))
+}
+
+bool UMGGameInstance::StartGame()
+{
+	if (GameMap.IsNull())
+		return false;
+	
+	GetWorld()->GetAuthGameMode()->bUseSeamlessTravel = true;
+
+	if (!GetWorld()->ServerTravel("/Game/MyContent/Maps/Lvl_Test?listen?GameMode=/Script/MGD_Template.GM_Battle"))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to travel to map"))
+		return false;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Travelling to map"))
+	return true;
+}
+
+bool UMGGameInstance::IsLoggedIn() const
+{
+	if (!IdentityInterface)
+		return false;
+
+	return IdentityInterface->GetLoginStatus(0) == ELoginStatus::LoggedIn;
+}
+
+bool UMGGameInstance::IsInSession() const
+{
+	if (!SessionInterface)
+		return false;
+
+	return SessionInterface->GetSessionState(TEXT("MGSESSION")) != EOnlineSessionState::NoSession;
+}
+
+FString UMGGameInstance::GetPlayerNickname() const
+{
+	if (!IsLoggedIn())
+		return TEXT("NOT LOGGED IN");
+
+	return IdentityInterface->GetPlayerNickname(0);
+}
+
 void UMGGameInstance::OnLoginComplete(int32 localUserNum, bool bWasSuccessful, const FUniqueNetId& UserId,
-	const FString& Error)
+                                      const FString& Error)
 {
 	BIEOnLoginComplete(bWasSuccessful, Error);
+}
+
+void UMGGameInstance::OnCreateSessionComplete(FName sessionName, bool success)
+{
+	if (!EnableListenServer(true))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to create listen server"))
+		SessionInterface->EndSession(sessionName);
+		success = false;
+	}
+
+	Delegate_OnHostGameComplete.Broadcast(success);
+
+	if (!success)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Session failed to create"))
+		return;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Successfully hosted session"))
+}
+
+void UMGGameInstance::OnInviteAccepted(const bool bWasSuccessful, const int32 ControllerId, FUniqueNetIdPtr UserId,
+                                       const FOnlineSessionSearchResult& InviteResult)
+{
+	if (!bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Invite failed to accept"))
+		return;
+	}
+	
+	if (!SessionInterface)
+		return;
+
+	SessionInterface->JoinSession(0, TEXT("MGSESSION"), InviteResult);
+}
+
+void UMGGameInstance::OnSessionJoinComplete(FName sessionName, EOnJoinSessionCompleteResult::Type joinResult)
+{
+	if (joinResult != EOnJoinSessionCompleteResult::Success)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to join session"))
+		return;
+	}
+
+	if (!SessionInterface)
+		return;
+
+	ClientTravelToSession(0, sessionName);
 }
--- Source/MGD_Template/Public/MGGameInstance.h
+++ Source/MGD_Template/Public/MGGameInstance.h
@@ -5,9 +5,12 @@
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "Interfaces/OnlineIdentityInterface.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "OnlineSubsystem.h"
 
 #include "MGGameInstance.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHostGameComplete, bool, success);
 
 /**
  * 
@@ -23,16 +26,44 @@ public:
 	UFUNCTION(BlueprintCallable, Category=Login)
 	void LoginToEOS();
 
+	UFUNCTION(BlueprintCallable, Category=Session)
+	void HostGame();
+
+	UFUNCTION(BlueprintCallable, Category=Session)
+	bool StartGame();
+	
+	UFUNCTION(BlueprintPure, Category=Login)
+	bool IsLoggedIn() const;
+
+	UFUNCTION(BlueprintPure, Category=Session)
+	bool IsInSession() const;
+
+	UFUNCTION(BlueprintPure, Category=Login)
+	FString GetPlayerNickname() const;
+
+	UPROPERTY(BlueprintAssignable, Category=Session)
+	FOnHostGameComplete Delegate_OnHostGameComplete;
+
 protected:
 	UFUNCTION(BlueprintImplementableEvent, Category=Login)
 	void BIEOnLoginComplete(bool success, const FString& error);
 	
 	// the login function that will run when the onlogincomplete delegate is run
 	void OnLoginComplete(int32 localUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
+
+	void OnCreateSessionComplete(FName sessionName, bool success);
+	void OnInviteAccepted(const bool bWasSuccessful, const int32 ControllerId, FUniqueNetIdPtr UserId, const FOnlineSessionSearchResult& InviteResult);
+	void OnSessionJoinComplete(FName sessionName, EOnJoinSessionCompleteResult::Type joinResult);
 	
 	// this will store a reference to the online subsystem
 	const IOnlineSubsystem* OssRef;
 
 	// this will store a reference to the identity interface in the online subsystem
 	IOnlineIdentityPtr IdentityInterface;
+	
+	IOnlineSessionPtr SessionInterface;
+
+	UPROPERTY(EditDefaultsOnly, Category=Maps)
+	TSoftObjectPtr<UWorld> GameMap;
+
 };
